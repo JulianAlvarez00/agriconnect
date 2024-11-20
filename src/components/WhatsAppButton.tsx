05120d3760b200f95e9a60019086e992acdd83f2
JulianAlvarez00 <julian.alvarez.ig@gmail.com>
2024-11-20 00:11:32 -0300

    minor fixes

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -6,7 +6,7 @@ interface WhatsAppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function WhatsAppButton({ children, variant = 'primary', ...props }: WhatsAppButtonProps) {
   const handleWhatsAppClick = () => {
-    window.open('https://wa.me/+5491112345678?text=Hola,%20me%20interesa%20saber%20más%20sobre%20la%20plataforma', '_blank');
+    window.open('https://wa.me/+5491136260707?text=Hola,%20me%20interesa%20saber%20más%20sobre%20la%20plataforma', '_blank');
   };
 
   const baseStyles = "inline-flex items-center px-8 py-4 font-semibold rounded-lg shadow-lg transform transition hover:scale-105";
